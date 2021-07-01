ad6d079fc2836e337b95edc4c61e45e7d76d5bd9
Aidar <aidarbekone@gmail.com>
2021-07-01 14:55:12 +0600

    added task

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // Подгоотовка к хакатону
+// / Дан объект: { hello: "World", console: "log", java: "script" }
+// Поместите в переменные, первые два свойства.
+// Следующий выведите в консоль. Используйте оператор ...rest (spread).
+
+// let objPro = { 
+//     hello: "World", 
+//     console: "log", 
+//     java: "script" 
+// };
+
+// let{hello, console, ...rest} = objPro;
+// console.log(hello);
+// console.log(second);
+// console.log(rest);
